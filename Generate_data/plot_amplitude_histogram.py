9527d16d5d99c05e8061791b0b8408a776d3b037
R2f2 <r24ocha@gmail.com>
2026-07-07 21:16:31 -0300

    colocando histograma sem log

--- Generate_data/plot_amplitude_histogram.py
+++ Generate_data/plot_amplitude_histogram.py
@@ -28,24 +28,24 @@ def plot_amplitude_histogram_from_h5(h5_path=DATA_PATH,out_dir=RUN_DIR,partes=50
     plt.grid(True, alpha=0.3)
     #plt.show()
 
-    out_path = os.path.join(out_dir, "log_amplitude_histogram.png")
-    plt.savefig(out_path, dpi=150, bbox_inches="tight")
-    plt.close()  # Fecha a figura para liberar memória
+    save_path = os.path.join(RUN_DIR, "histograma_amplitudes_preditas.pdf")
+    plt.savefig(save_path, dpi=150, bbox_inches='tight')
+    plt.close()
 
 
     # Plotar histograma
-    plt.figure(figsize=(8, 5))
-    plt.hist(np.log10(amplitudes + 1e-10), bins=200, edgecolor='black', alpha=0.7)
-   # plt.yscale('log')
+    plt.figure(figsize=(10, 6))
+    plt.hist((amplitudes + 1e-10), bins=100, alpha=0.7, color='red', edgecolor='black')
     plt.xlabel("Amplitude Amplitude (max |S_nl|)")
     plt.ylabel("Contagem")
     plt.title(f"Distribuição das log_Amplitudes - {h5_path}")
     plt.grid(True, alpha=0.3)
+    plt.tight_layout()
     #plt.show()
 
-    out_path = os.path.join(out_dir, "amplitude_histogram.png")
-    plt.savefig(out_path, dpi=150, bbox_inches="tight")
-    plt.close() 
+    save_path = os.path.join(RUN_DIR, "histograma_amplitudes_preditas.pdf")
+    plt.savefig(save_path, dpi=150, bbox_inches='tight')
+    plt.close()
     
     return log_amplitudes
 
