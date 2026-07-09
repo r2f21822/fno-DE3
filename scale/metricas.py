8405ba3327228a96715412c251026d95db51b28e
R2f2 <r24ocha@gmail.com>
2026-07-09 13:53:29 -0300

    arrumando metricas

--- scale/metricas.py
+++ scale/metricas.py
@@ -14,21 +14,40 @@ def metricas(results_dir):
   
     all_pred_exp = 10 ** all_pred
     all_true_exp = 10 ** all_true
+
+    #METRICAS COM LOG (A)
     
-
-    print("log(A) - Erro Medio Absoluto: ",np.mean(np.abs(all_true - all_pred)))  # erro médio absoluto
-    print("log(A) - Erro Quadratico Médio :",np.mean((all_true - all_pred) ** 2))   
-    print("log(A) - Raiz Erro Quadratico Medio",np.sqrt(np.mean((all_true - all_pred) ** 2)))             
-
-
-    print("A - Erro Medio Absoluto: ",np.mean(np.abs(all_true_exp - all_pred_exp)))  
-    print("A - Erro Quadratico Médio :",np.mean((all_true_exp - all_pred_exp) ** 2))   
-    print("A - Raiz Erro Quadratico Medio",np.sqrt(np.mean((all_true_exp - all_pred_exp) ** 2))) 
+    mae = np.mean(np.abs(all_true - all_pred))
+    mse = np.mean((all_true - all_pred) ** 2)
+    rmse = np.sqrt(mse)
+    mape = np.mean(np.abs((all_true - all_pred) / (all_true+ 1e-8))) * 100
+    rmse_percentual = (rmse / np.mean(all_true)) * 100
     
-    rmse_percentual = (np.sqrt(np.mean((all_true_exp - all_pred_exp) ** 2)) / np.mean(all_true_exp)) * 100
-    print(f"RMSE percentual: {rmse_percentual}%")
+    print(f"log (A): MAE  (Erro Absoluto Médio):              {mae:.6f}")
+    print(f"log (A): MSE  (Erro Quadrático Médio):            {mse:.6f}")
+    print(f"log (A): RMSE (Raiz do Erro Quadrático Médio):    {rmse:.6f}")
+    print(f"log (A): MAPE (Erro Percentual Absoluto Médio):   {mape:.2f}%")
+    print(f"log (A): RMSE% (RMSE Percentual):                 {rmse_percentual:.2f}%")         
 
 
+    # METRICAS COM A AMPLITUDE REAL
+    print("\n" + "-"*60 + "\n")
+    mae = np.mean(np.abs(all_true_exp - all_pred_exp))
+    mse = np.mean((all_true_exp - all_pred_exp) ** 2)
+    rmse = np.sqrt(mse)
+    mape = np.mean(np.abs((all_true_exp - all_pred_exp) / (all_true_exp + 1e-8))) * 100
+    rmse_percentual = (rmse / np.mean(all_true_exp)) * 100
+    
+    print(f"A: MAE  (Erro Absoluto Médio):              {mae:.6f}")
+    print(f"A: MSE  (Erro Quadrático Médio):            {mse:.6f}")
+    print(f"A: RMSE (Raiz do Erro Quadrático Médio):    {rmse:.6f}")
+    print(f"A: MAPE (Erro Percentual Absoluto Médio):   {mape:.2f}%")
+    print(f"A: RMSE% (RMSE Percentual):                 {rmse_percentual:.2f}%")
+
+
+#METRICAS COM AS MEDIANAS
+
+    print("\n" + "-"*60 + "\n")
     mediana_real = np.median(all_true_exp)
     media_real = np.mean(all_true_exp)
 
@@ -47,44 +66,43 @@ def metricas(results_dir):
 
     print("Metricas olhando metade () das amostras")
 
-#METADE MENOR (amplitude<=mediana)
+    #METADE MENOR (amplitude<=mediana)
+
+    print("\n" + "-"*60 + "\n")
     mask_menor = all_true_exp <= mediana_real
     true_menor = all_true_exp[mask_menor]
     pred_menor = all_pred_exp[mask_menor]
-
+    
     n_menor = len(true_menor)
     mae_menor = np.mean(np.abs(true_menor - pred_menor))
     rmse_menor = np.sqrt(np.mean((true_menor - pred_menor) ** 2))
     mape_menor = np.mean(np.abs((true_menor - pred_menor) / (true_menor + 1e-8))) * 100
-
-
-    print("\n" + "-"*60)
-    print("METADE MENOR (amplitudes <= mediana)")
-    print(f"Número de amostras:",n_menor)
-    print(f"Faixa de amplitude: {true_menor.min():.6f} a {true_menor.max():.6f}")
-    print(f"A - Erro Medio Absoluto:        {mae_menor:.6f}")
-    print(f"A - Erro Quadratico Médio :     {rmse_menor:.6f}")
-    print(f"A - Raiz Erro Quadratico Medio: {mape_menor:.2f}%")
+    
+    print("\n METADE MENOR (amplitudes <= mediana)")
+    print(f"  Número de amostras:        {n_menor}")
+    print(f"  Faixa de amplitude:        {true_menor.min():.6f} a {true_menor.max():.6f}")
+    print(f"  MAE  (Erro Absoluto Médio):        {mae_menor:.6f}")
+    print(f"  RMSE (Raiz do Erro Quadrático):    {rmse_menor:.6f}")
+    print(f"  MAPE (Erro Percentual Médio):      {mape_menor:.2f}%")
 
     # METADE MAIOR (amplitudes > mediana)
 
-
+    print("\n" + "-"*60 + "\n")
     mask_maior = all_true_exp > mediana_real
     true_maior = all_true_exp[mask_maior]
     pred_maior = all_pred_exp[mask_maior]
-
+    
     n_maior = len(true_maior)
     mae_maior = np.mean(np.abs(true_maior - pred_maior))
     rmse_maior = np.sqrt(np.mean((true_maior - pred_maior) ** 2))
     mape_maior = np.mean(np.abs((true_maior - pred_maior) / (true_maior + 1e-8))) * 100
-
-    print("\n" + "-"*60)
-    print("METADE MAIOR (amplitudes > mediana)")
-    print(f"Número de amostras:",n_maior)
-    print(f"Faixa de amplitude: {true_maior.min():.6f} a {true_maior.max():.6f}")
-    print(f"A - Erro Medio Absoluto:        {mae_maior:.6f}")
-    print(f"A - Erro Quadratico Médio :     {rmse_maior:.6f}")
-    print(f"A - Raiz Erro Quadratico Medio: {mape_maior:.2f}%")
+    
+    print("\n METADE MAIOR (amplitudes > mediana)")
+    print(f"  Número de amostras:        {n_maior}")
+    print(f"  Faixa de amplitude:        {true_maior.min():.6f} a {true_maior.max():.6f}")
+    print(f"  MAE  (Erro Absoluto Médio):        {mae_maior:.6f}")
+    print(f"  RMSE (Raiz do Erro Quadrático):    {rmse_maior:.6f}")
+    print(f"  MAPE (Erro Percentual Médio):      {mape_maior:.2f}%")
 
 
 
