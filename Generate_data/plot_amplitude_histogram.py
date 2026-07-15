45b39d4eab6dc121dbf39c32ad2cb89045c258f8
R2f2 <r24ocha@gmail.com>
2026-07-15 16:21:08 -0300

    Add files via upload

---
+++ Generate_data/plot_amplitude_histogram.py
@@ -0,0 +1,53 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from fno.fno_run_training import load_snl_dataset
+EPS_NORM = 1e-8
+
+DATA_PATH = "Generate_data/snl/snl_dataset.h5"
+RUN_DIR   = "Generate_data/figures"
+
+def plot_amplitude_histogram_from_h5(h5_path=DATA_PATH,out_dir=RUN_DIR,partes=50):
+   
+
+    X, Y = load_snl_dataset(h5_path)
+
+    amplitudes = Y.abs().amax(dim=(1, 2, 3)).numpy()
+    log_amplitudes = np.log10(amplitudes + 1e-10)  # log10 de a
+
+    import os
+    os.makedirs(out_dir, exist_ok=True)
+ 
+    # Plotar histograma
+    plt.figure(figsize=(8, 5))
+    plt.hist(log_amplitudes, bins=partes, edgecolor='black', alpha=0.7)
+   # plt.yscale('log')
+    plt.xlabel("log amplitude Amplitude (max |S_nl|)")
+    plt.ylabel("Contagem")
+    plt.title(f"Distribuição das log_Amplitudes - {h5_path}")
+    plt.grid(True, alpha=0.3)
+    #plt.show()
+
+    save_path = os.path.join(RUN_DIR, "histograma_amplitudes_preditas.pdf")
+    plt.savefig(save_path, dpi=150, bbox_inches='tight')
+    plt.close()
+
+
+    # Plotar histograma
+    plt.figure(figsize=(10, 6))
+    plt.hist((amplitudes + 1e-10), bins=100, alpha=0.7, color='red', edgecolor='black')
+    plt.xlabel("Amplitude Amplitude (max |S_nl|)")
+    plt.ylabel("Contagem")
+    plt.title(f"Distribuição das log_Amplitudes - {h5_path}")
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+    #plt.show()
+
+    save_path = os.path.join(RUN_DIR, "histograma_amplitudes_preditas.pdf")
+    plt.savefig(save_path, dpi=150, bbox_inches='tight')
+    plt.close()
+    
+    return log_amplitudes
+
+if __name__ == "__main__":
+    amps = plot_amplitude_histogram_from_h5()
